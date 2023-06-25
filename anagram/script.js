a6fe6253b59ce6b85b36bf77cc3f6d7d8a95da9a
Igor <igor132143@gmail.com>
2023-06-25 14:04:08 +0300

    add a new js-problems anagram

--- anagram/script.js
+++ anagram/script.js
@@ -1,34 +1,31 @@
-
-
-function buildCharObject(str) {
-    const charObj = {};
-    str = str.toLowerCase().replace(/[^\w]/g);
-    for (let char of str) {
-        charObj[char] = charObj[char] + 1 || 1;
-    };
-    return charObj;
-};
-
 function anagram(strA, strB) {
-    const aCharObj = buildCharObject(strA);
-    const bCharObj = buildCharObject(strB);
+    strA = strA.toLowerCase().replace(/\s/g, '');
+    strB = strB.toLowerCase().replace(/\s/g, '');
 
-    if (Object.keys(aCharObj).length !== Object.keys(bCharObj).length) {
-        return false;
+    if (strA.length !== strB.length) {
+        return `It isn't an anagram. Length isn't the same`;
     };
 
-    for (let char in aCharObj) {
-        if (aCharObj[char] !== bCharObj[char]) {
-            return false;
-        };
+    for (let i = 0; i < strA.length; i++) {
+        const charA = strA[i];
+        const charIndexB = strB.indexOf(charA);
+        const isCharAPresentInStrB = charIndexB >= 0;
+
+        if (isCharAPresentInStrB) {
+            strB = strB.replace(charA, '');
+        } else {
+            return `It isn't an anagram. Second word doesn't have ${charA}`;
+        }
     };
-    return true;
+
+    return `It is an anagram`
 };
+
 
 console.log(anagram('friend', 'Finder'));
 console.log(anagram('hello', 'bye'));
 
-const anagram2 = (a,b) => [...a.toLowerCase()].sort().toString() === [...b.toLowerCase()].sort().toString();
+// const anagram2 = (a,b) => [...a.toLowerCase()].sort().toString() === [...b.toLowerCase()].sort().toString();
 
-console.log(anagram2('friend', 'Finder'));
-console.log(anagram2('hello', 'bye'));
+// console.log(anagram2('friend', 'Finder'));
+// console.log(anagram2('hello', 'bye'));
