8c046f341fd294dc7fa9ed6c261ee61c310cac94
Igor <igor132143@gmail.com>
2023-08-07 19:21:40 +0300

    added a new js-problem: create-phone-number

---
+++ create-phone-number/script.js
@@ -0,0 +1,18 @@
+function createPhoneNumber(num) {
+    const numStr = num.toString().replace(/\D/g, '');
+
+    if (numStr.length !== 10) return 'Phone number is wrong';
+
+    const numArr = numStr.split('');
+
+    numArr.splice(0,0,'(');
+    numArr.splice(4,0,')');
+    numArr.splice(5,0,' ');
+    numArr.splice(9,0,'-');
+    
+    return numArr.join(''); 
+}
+
+console.log(createPhoneNumber(2343216781)); // -> (234) 321-6781
+console.log(createPhoneNumber(23432)); // -> Phone number is wrong
+console.log(createPhoneNumber('234a216781')); // -> Phone number is wrong
